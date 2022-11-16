4f70a892266b00a58fd5c3b2d91ea3c0671d0683
fanzhefu <68694994+fanzhefu@users.noreply.github.com>
2022-11-16 10:24:59 -0500

    Update actors.py

--- actors.py
+++ actors.py
@@ -1,9 +1,14 @@
+# -*- coding: utf-8 -*-
 """
+Created on Wed Nov 16 09:01:22 2022
+
 this snippet is used to 
 1.scrapping https://malpedia.caad.fkie.fraunhofer.de/actors
    to get actor's name, aka, synonym, and the link to the description page.
 2. go through each description page to extract actor's description'
-3. write all actor's name, aka, synonym and description to actors.json file
+3. write all actor's name, aka, synonym and description to actors.json file.
+@author: fan.z
+
 """
 import requests
 from bs4 import BeautifulSoup
@@ -16,14 +21,13 @@ page = requests.get(URL+'/'+ACTORS)
 soup = BeautifulSoup(page.content, "html.parser")   
 
 names = [name.text for name in soup.find_all(class_="common_name")]
-akas = [ aka['title'] if (aka := row.find(class_='fa fa-info-circle')) != None else 'aka: N/A' for row in soup.find_all(class_="clickable-row")]
+akas = [ aka['title'].lstrip('aka: ') if (aka := row.find(class_='fa fa-info-circle')) != None else 'aka: N/A' for row in soup.find_all(class_="clickable-row")]
 synonyms = [ (synonym:= row.find(class_='synonyms')).text  for row in soup.find_all(class_="clickable-row")]
 links = [URL+link['data-href'] for link in soup.find_all( class_="clickable-row")]
 
-i = 0
-total = len(names) #just a counter 
+i, total = 0, len(names) #just a counter 
 descriptions = []
-print(f"There are total {total} acotors, it will take time, wait until done ... ...\n")
+print(f"\nThere are total {total} acotors, it will take time, wait until done ... ...\n")
 for link in links:
     # go through each page to extract the description 
     page = requests.get(link)
@@ -37,4 +41,4 @@ actors =[{'name':name, 'aka':aka, 'synonym':synonym, 'description':description} 
 with open('actors.json', 'w', encoding='utf-8', newline='\r\n') as f:
     json.dump(actors, f, ensure_ascii=False, indent=4) #
     
-print("\nDone ... ...")
+print("\n\nDone, check output file: actors.json ... ...")
