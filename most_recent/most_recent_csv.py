7bfc3558ee7df8cbff0b0716c0f3756dd0049681
fanzhefu <68694994+fanzhefu@users.noreply.github.com>
2023-05-19 19:30:42 -0400

    Add files via upload

---
+++ most_recent/most_recent_csv.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+INPUT = 'description.txt'   # Input file name
+OUTPUT = 'most_recent.csv'  # Output file name
+
+name, most_recent = '', []
+
+for row in open('description.txt', 'r', encoding='utf-8').readlines()[::-1]:
+    row_lst = row.rstrip('","\n').split('"')
+    if row_lst[-1] != name:
+        most_recent.append(row_lst[-1]+',"'+''.join(row_lst[:-3])+'"\n')
+        name = row_lst[-1]
+
+open(OUTPUT, 'w', encoding='utf-8').writelines(most_recent[::-1])
+print('Done... ... ')
