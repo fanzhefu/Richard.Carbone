b46ab1400b421fdd8e8a47d75dc82f581d0b5d22
fanzhefu <68694994+fanzhefu@users.noreply.github.com>
2023-05-25 11:56:21 -0400

    Add files via upload

---
+++ actor_attack_patterns/extract_patterns.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/env python
+import os
+
+INPUT_DIR = 'json_files'
+OUTPUT_DIR = 'result'
+
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+
+for file in os.listdir((INPUT_DIR)):
+    text = open(INPUT_DIR+'\\'+file, 'r', encoding='utf-8').readlines()[-1]
+    open(OUTPUT_DIR+'\\'+file, 'w', encoding='utf-8').write(text)
+
+print('\n Done... ...')
