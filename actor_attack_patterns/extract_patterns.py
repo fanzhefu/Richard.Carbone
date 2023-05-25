d994261dbdde478facd6016231cb04a5ec55cc74
fanzhefu <68694994+fanzhefu@users.noreply.github.com>
2023-05-25 12:55:24 -0400

    Update extract_patterns.py

--- actor_attack_patterns/extract_patterns.py
+++ actor_attack_patterns/extract_patterns.py
@@ -8,7 +8,7 @@ OUTPUT_DIR = 'result'
 if not os.path.exists(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR)
 
-for file in os.listdir((INPUT_DIR)):
+for file in os.listdir(INPUT_DIR):
     text = open(INPUT_DIR+'\\'+file, 'r', encoding='utf-8').readlines()[-1]
     open(OUTPUT_DIR+'\\'+file, 'w', encoding='utf-8').write(text)
 
