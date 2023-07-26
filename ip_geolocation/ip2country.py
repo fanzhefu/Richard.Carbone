197d0882eca0869038fbee61740cfb06f1132540
fanzhefu <68694994+fanzhefu@users.noreply.github.com>
2023-07-26 10:53:42 -0400

    Add files via upload

--- ip_geolocation/ip2country.py
+++ ip_geolocation/ip2country.py
@@ -1,14 +1,22 @@
-import csv
-from netaddr import *
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 25 11:32:15 2023
 
-with open("GeoIPCountryWhois.csv") as csv_file:
-    read_csv = csv.reader(csv_file, delimiter=",")
+@author: fanzhefu
+"""
+
+import csv
+#from ipaddress import *
+import netaddr
+
+with open("GeoIPCountryWhois.csv", encoding='utf-8') as csv_file:
+    rows = csv.reader(csv_file, delimiter=",")
     what_ip = input("What IP should be looked up? ")
 
-    for row in read_csv:
-        ip_low = IPAddress(row[0])
-        ip_high = IPAddress(row[1])
+    for row in rows:
+        ip_low = netaddr.IPAddress(row[0])
+        ip_high = netaddr.IPAddress(row[1])
         country = row[4]
 
-        if IPAddress(what_ip) >= ip_low and IPAddress(what_ip) <= ip_high:
+        if ip_low <= netaddr.IPAddress(what_ip) <= ip_high:
             print(country)
--- ip_geolocation/ip2ctry.py
+++ ip_geolocation/ip2ctry.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Jul 25 09:19:24 2023
 
-@author: fan.z
+@author: fanzhefu
 """
 
 import csv
@@ -11,30 +11,32 @@ import struct
 
 from bisect import bisect_left
 
-def ip2long(ip):
+
+def ip2long(ip_addr):
     """
-    Convert an IP string to long (see http://stackoverflow.com/a/9591005/21945)
+    Convert an IP string to long 
     """
-    packedIP = socket.inet_aton(ip)
-    return struct.unpack("!L", packedIP)[0]
+    packed_ip = socket.inet_aton(ip_addr)
+    return struct.unpack("!L", packed_ip)[0]
 
 
-class GeoIPCountry(object):
+class GeoIPCountry():
     def __init__(self, geoips_file):
         """
         Load IP range location map from CSV file filling in any empty ranges as
         we go. Assumes that the data in geoips_file is sorted by IP address.
         """
-        r = csv.reader(geoips_file)
+        rows = csv.reader(geoips_file)
         self.geoips = []
         last_hi = 0
-        for row in r:
+        for row in rows:
             if int(row[2]) != last_hi+1:
                 self.geoips.append((last_hi+1, int(row[2])-1, None, None))
             self.geoips.append((int(row[2]), int(row[3]), row[4], row[5]))
             last_hi = int(row[3])
         if last_hi < ip2long('255.255.255.255'):
-            self.geoips.append((last_hi+1, ip2long('255.255.255.255'), None, None))
+            self.geoips.append(
+                (last_hi+1, ip2long('255.255.255.255'), None, None))
         self.keys = [geoip[1] for geoip in self.geoips]
 #        assert sorted(self.keys) == self.keys
 
@@ -46,13 +48,14 @@ class GeoIPCountry(object):
 
 
 if __name__ == '__main__':
-    with open('GeoIPCountryWhois.csv') as geoips_file:
-        geoip = GeoIPCountry(geoips_file)
+    with open('GeoIPCountryWhois.csv',encoding='utf-8') as csv_file:
+        geoip = GeoIPCountry(csv_file)
 
-    for ip_address in ('0.1.2.3', '1.2.3.4', '192.168.1.1', '203.123.4.23',
+    for ip in ('0.1.2.3', '1.2.3.4', '192.168.1.1', '203.123.4.23',
                        '123.132.123.123', '223.255.255.255', '255.255.255.255'):
-        country = geoip.lookup_country(ip_address)
+        country = geoip.lookup_country(ip)
         if country[0] is not None:
-            print("{:<15} -> {} ({})".format(ip_address, country[1], country[0]))
+            print("{:<15} -> {} ({})".format(ip,
+                  country[1], country[0]))
         else:
-            print("{:<15} -> unknown".format(ip_address))
+            print("{:<15} -> unknown".format(ip))
