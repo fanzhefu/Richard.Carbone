271018fb3e686fc95c6b42f87d2c0d25fb76c54d
fanzhefu <68694994+fanzhefu@users.noreply.github.com>
2023-08-04 17:25:02 -0400

    Add files via upload

---
+++ nvdcve/apiv2/dl_cve_apiv2.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  4 17:05:41 2023
+
+@author: fan.z
+"""
+
+import datetime
+import json
+import requests
+
+YEAR = '2023'
+
+# The maximum allowable range when using any date range parameters is 120 consecutive days.
+BASE_URL = 'https://services.nvd.nist.gov/rest/json/cves/2.0'
+pub_start_date = [YEAR+'-'+'01'+'-01', YEAR+'-'+'04' +
+                  '-01', YEAR+'-'+'07'+'-01', YEAR+'-'+'10'+'-01']
+pub_end_date = [YEAR+'-'+'04'+'-01', YEAR+'-'+'07' +
+                '-01', YEAR+'-'+'10'+'-01', YEAR+'-'+'01'+'-01']
+
+print('\nDownloading ... ...')
+for i in range(4):
+    if datetime.datetime.now() > datetime.datetime(*map(int, pub_start_date[i].split('-'))):
+        #         print(i)
+        params = {"pubStartDate": pub_start_date[i]+'T00:00:00.000',
+                  "pubEndDate": pub_end_date[i]+'T00:00:00.000'}
+        response = requests.get(BASE_URL, params).json()
+        vulns = response['vulnerabilities']
+
+        for vuln in vulns:
+            print(vuln['cve']['id'])
+            with open(YEAR+'.json', 'a', encoding='utf-8') as f:
+                json.dump(vuln, f, ensure_ascii=False, indent=4)
+
+print('\nDone, check the file "'+YEAR+'.json" for all the CVEs')
