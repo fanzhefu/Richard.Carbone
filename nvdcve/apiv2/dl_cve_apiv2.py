8ca43af0c0799e94c4558b1add417b68173d6cdd
fanzhefu <68694994+fanzhefu@users.noreply.github.com>
2023-08-08 10:30:48 -0400

    Add files via upload

--- nvdcve/apiv2/dl_cve_apiv2.py
+++ nvdcve/apiv2/dl_cve_apiv2.py
@@ -2,44 +2,61 @@
 """
 Created on Fri Aug  4 17:05:41 2023
 
+download the nvd cves of nist.gov by year
+
 @author: fan.z
 """
-
-import datetime
+import datetime as dt
 import json
 import requests
 
-# Change the year for yourself
-YEAR = '2023'
+# The year of all the CVEs to be downloaded.
+YEAR = 2003
 
 BASE_URL = 'https://services.nvd.nist.gov/rest/json/cves/2.0?'
 API_KEY = '168227f8-1383-4874-8275-470c4a96855e'
 
-def dl_cves(YEAR):
+
+def dl_cves(year: str):
+    '''
+    Parameters
+    ----------
+    year : str
+        The year of the CVEs to be downloaded.
+
+    Returns
+    -------
+    None.
+
+    '''
     # The maximum allowable range when using any date range parameters is 120 consecutive days.
-    pub_start_date = [YEAR+'-'+'01'+'-01', YEAR+'-'+'04' +
-                      '-01', YEAR+'-'+'07'+'-01', YEAR+'-'+'10'+'-01']
-    pub_end_date = [YEAR+'-'+'04'+'-01', YEAR+'-'+'07' +
-                    '-01', YEAR+'-'+'10'+'-01', YEAR+'-'+'01'+'-01']
-    
+    pub_start_date = [year+'-01-01T00:00:00.000', year+'-04-01T00:00:00.000',
+                      year+'-07-01T00:00:00.000', year+'-10-01T00:00:00.000']
+    pub_end_date = [year+'-04-01T00:00:00.000', year+'-07-01T00:00:00.000',
+                    year+'-10-01T00:00:00.000', year+'-12-31T23:59:59.999']
+
     print('\nDownloading ... ...')
     for i in range(4):
-        if datetime.datetime.now() > datetime.datetime(*map(int, pub_start_date[i].split('-'))):
-            #
-            params = {"pubStartDate": pub_start_date[i]+'T00:00:00.000',
-                      "pubEndDate": pub_end_date[i]+'T00:00:00.000'}
+        if dt.datetime.now() > dt.datetime(*map(int, pub_start_date[i][:10].split('-'))):
+
+            params = {"pubStartDate": pub_start_date[i],
+                      "pubEndDate": pub_end_date[i]}
             headers = {'content-type': 'application/json', 'apiKey': API_KEY}
-    
-            response = requests.get(BASE_URL, params=params,
-                                    headers=headers).json()
-            vulns = response['vulnerabilities']
-    
+
+            response = requests.get(
+                BASE_URL, params=params, headers=headers, timeout=30)
+
+            print(response.status_code)
+            #response.encoding = 'utf-8'
+            vulns = response.json()['vulnerabilities']
+
             for vuln in vulns:
                 print(vuln['cve']['id'], end=', ')
-                with open(YEAR+'.json', 'a', encoding='utf-8') as f:
-                    json.dump(vuln, f, ensure_ascii=False, indent=4)
-    
-    print('\n\nDone, check the file "'+YEAR+'.json" for all the CVEs')
+                with open(year+'.json', 'a', encoding='utf-8') as f_json:
+                    json.dump(vuln, f_json, ensure_ascii=False, indent=4)
+
+    print('\n\nDone, check the file "'+year+'.json" for all the CVEs')
+
 
 if __name__ == "__main__":
-    dl_cves(YEAR)
+    dl_cves(str(YEAR))
